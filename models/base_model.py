9458f1053638604830934d8273018758c1c75a2d
afradZ <afradzakaria@gmail.com>
2024-11-17 03:29:24 -0800

    updated some models

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,12 @@
 import uuid
 import models
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class BaseModel:
-    """This class will defines all common attributes/methods
+    """This class defines all common attributes/methods
     for other classes
     """
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -34,15 +26,14 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-                if 'id' not in kwargs:
-                    self.id = str(uuid.uuid4())
-                if 'created_at' not in kwargs:
-                    self.created_at = datetime.now()
-
-                if 'created_at' in kwargs and 'updated_at' not in kwargs:
-                    self.updated_at = self.created_at
-                else:
-                    self.updated_at = datetime.now()
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'created_at' in kwargs and 'updated_at' not in kwargs:
+                self.updated_at = self.created_at
+            else:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -56,7 +47,7 @@ class BaseModel:
             type(self).__name__, self.id, self.__dict__)
 
     def __repr__(self):
-        """returns a string representaion
+        """returns a string representation
         """
         return self.__str__()
 
@@ -68,22 +59,18 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """creates a dictionary of the class  and returns
+        """creates a dictionary of the class and returns
         a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-
-        if my_dict['_sa_instance_state']:
-            my_dict.pop('_sa_instance_state')
-
         return my_dict
 
     def delete(self):
         """Deletes current instance from
         the model storage
         """
-
         models.storage.delete(self)
+
